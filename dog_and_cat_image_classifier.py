01ba96f6b5dbcec23d70dd7ec5ed485e42f7c7b6
Manvi Pandey <manvi.muskan@gmail.com>
2020-09-13 15:36:43 +0530

    Loading dataset

--- dog_and_cat_image_classifier.py
+++ dog_and_cat_image_classifier.py
@@ -1,3 +1,4 @@
+# importing libraries
 import tensorflow as tf
 
 from tensorflow.keras.models import Sequential
@@ -7,3 +8,18 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+
+# extracting training and test datasets 
+URL = 'https://cdn.freecodecamp.org/project-data/cats-and-dogs/cats_and_dogs.zip'
+
+path_to_zip = tf.keras.utils.get_file('cats_and_dogs.zip', origin=URL, extract=True)
+
+PATH = os.path.join(os.path.dirname(path_to_zip), 'cats_and_dogs')
+
+train_dir = os.path.join(PATH, 'train')
+validation_dir = os.path.join(PATH, 'validation')
+test_dir = os.path.join(PATH, 'test')
+
+
+
+
